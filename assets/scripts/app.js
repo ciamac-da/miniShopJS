cf27c82a4d0079bed1fb23e66bc318ad0eae4e6e
ciamac-da <ciamac.davoudi@gmail.com>
2020-10-19 20:07:59 +0200

    verified by Cia v1.0.2 product list

---
+++ assets/scripts/app.js
@@ -0,0 +1,17 @@
+
+const productsList ={
+     products =[
+        {
+            title: "The last of us II",
+    imageUrl:"https://images-na.ssl-images-amazon.com/images/I/71UDXCGb8cL._AC_SY679_.jpg",
+    price : 69.99,
+    description:"Action-Adventure"
+},
+{
+    title: "Metro Exodus",
+    imageUrl:"https://cdn.vox-cdn.com/thumbor/aRM2buml6QShnavgszXDLn9IcJo=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19702068/_assets_Uploads_MetroExodus_SUMMER_WallPaper_1920x1200.jpg",
+    price : 49.99,
+    description:"Action-Adventure"
+},
+]
+}
