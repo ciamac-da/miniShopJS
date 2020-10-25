b3091e3773720474166610296b00d7085ed31d04
ciamac-da <ciamac.davoudi@gmail.com>
2020-10-25 20:12:27 +0100

    Verified by Cia v1.1.6

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,175 +1,187 @@
 class Product {
-    // title = 'DEFAULT';
-    // imageUrl;
-    // description;
-    // price;
-  
-    constructor(title, image, desc, price) {
-      this.title = title;
-      this.imageUrl = image;
-      this.description = desc;
-      this.price = price;
-    }
-  }
-  
-  class ElementAttribute {
-    constructor(attrName, attrValue) {
-      this.name = attrName;
-      this.value = attrValue;
-    }
-  }
-  
-  class Component {
-    constructor(renderHookId, shouldRender = true) {
-      this.hookId = renderHookId;
-      if(shouldRender){
-        this.render();
-      }
-    }
-  
-    createRootElement(tag, cssClasses, attributes) {
-      const rootElement = document.createElement(tag);
-      if (cssClasses) {
-        rootElement.className = cssClasses;
-      }
-      if (attributes && attributes.length > 0) {
-        for (const attr of attributes) {
-          rootElement.setAttribute(attr.name, attr.value);
-        }
-      }
-      document.getElementById(this.hookId).append(rootElement);
-      return rootElement;
-    }
-  }
-  
-  class ShoppingCart extends Component {
-    items = [];
-  
-    set cartItems(value) {
-      this.items = value;
-      this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
-    }
-  
-    get totalAmount() {
-      const sum = this.items.reduce(
-        (prevValue, curItem) => prevValue + curItem.price,
-        0
-      );
-      return sum;
-    }
-  
-    constructor(renderHookId) {
-      super(renderHookId);
-    }
-  
-    addProduct(product) {
-      const updatedItems = [...this.items];
-      updatedItems.push(product);
-      this.cartItems = updatedItems;
-    }
-  
-    render() {
-      const cartEl = this.createRootElement('section', 'cart');
-      cartEl.innerHTML = `
-        <h2>Total: \$${0}</h2>
-        <button>Order Now!</button>
-      `;
-      this.totalOutput = cartEl.querySelector('h2');
-    }
-  }
-  
-  class ProductItem extends Component {
-    constructor(product, renderHookId) {
-      super(renderHookId, false)
-      this.product = product;
-      this.render()
-    }
-  
-    addToCart() {
-      App.addProductToCart(this.product);
-    }
-  
-    render() {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-          <div>
-            <img src="${this.product.imageUrl}" alt="${this.product.title}" >
-            <div class="product-item__content">
-              <h2>${this.product.title}</h2>
-              <h3>\$${this.product.price}</h3>
-              <p>${this.product.description}</p>
-              <button>Add to Cart</button>
-            </div>
-          </div>
-        `;
-      const addCartButton = prodEl.querySelector('button');
-      addCartButton.addEventListener('click', this.addToCart.bind(this));
-      return prodEl;
-    }
-  }
-  
-  class ProductList extends Component {
-    products = [];
-    constructor(renderHookId) {
-      super(renderHookId)
-      this.fetchProducts();
-    }
+  // title = 'DEFAULT';
+  // imageUrl;
+  // description;
+  // price;
 
-    fetchProducts() {
-      this.products = [
-        new Product(
-          'A Pillow',
-          'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
-          'A soft pillow!',
-          19.99
-        ),
-        new Product(
-          'A Carpet',
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
-          'A carpet which you might like - or not.',
-          89.99
-        )
-      ];
-      this.renderProducts();
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId, shouldRender = true) {
+    this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
     }
-  
-    renderProducts(){
-      for (const prod of this.products) {
-        new ProductItem(prod, "prod-list");
+  }
+
+  render() {}
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
       }
     }
-    
-    render() {
-      this.createRootElement("ul", "product-list", [new ElementAttribute("id", "prod-list")])
-    if(this.products && this.products.length > 0){
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
+  items = [];
+
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log('Ordering...');
+      console.log(this.items);
+    };
+    this.render();
+  }
+
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+
+  render() {
+    const cartEl = this.createRootElement('section', 'cart');
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    const orderButton = cartEl.querySelector('button');
+    // orderButton.addEventListener('click', () => this.orderProducts());
+    orderButton.addEventListener('click', this.orderProducts);
+    this.totalOutput = cartEl.querySelector('h2');
+  }
+}
+
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
+    this.product = product;
+    this.render();
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodEl = this.createRootElement('li', 'product-item');
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
+  }
+}
+
+class ProductList extends Component {
+  #products = [];
+
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.render();
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.#products = [
+      new Product("The last of us II", 
+      "https://img.youtube.com/vi/ZyDOeshZFfg/maxresdefault.jpg",
+      "Action-Adventure",
+      69.99,
+      ),
+      new Product("Metro Exodus", 
+      "https://cdn.vox-cdn.com/thumbor/aRM2buml6QShnavgszXDLn9IcJo=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19702068/_assets_Uploads_MetroExodus_SUMMER_WallPaper_1920x1200.jpg",
+      "Action-Adventure",
+      49.99,
+      ),
+];
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    for (const prod of this.#products) {
+      new ProductItem(prod, 'prod-list');
+    }
+  }
+
+  render() {
+    this.createRootElement('ul', 'product-list', [
+      new ElementAttribute('id', 'prod-list')
+    ]);
+    if (this.#products && this.#products.length > 0) {
       this.renderProducts();
     }
-    }
   }
-  
-  class Shop extends Component{
-    constructor(){
-      super()
-    }
-    render() {
-      this.cart = new ShoppingCart('app');
-      new ProductList("app");
-    }
+}
+
+class Shop {
+  constructor() {
+    this.render();
   }
-  
-  class App {
-    static cart;
-  
-    static init() {
-      const shop = new Shop();
-      this.cart = shop.cart;
-    }
-  
-    static addProductToCart(product) {
-      this.cart.addProduct(product);
-    }
+
+  render() {
+    this.cart = new ShoppingCart('app');
+    new ProductList('app');
   }
-  
-  App.init();
-  
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
